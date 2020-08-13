d31a17dddcbe7cc8e961c88ca5a5e410018b5498
Your Name <ck_ding>
2020-08-13 17:45:27 +0800

    'antd'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ConfigProvider } from "antd";
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from "antd/es/locale/zh_CN";
+import 'moment/locale/zh-cn';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ConfigProvider locale={zhCN}>
+    {/* <React.StrictMode> */}
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    {/* </React.StrictMode> */}
+  </ConfigProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
